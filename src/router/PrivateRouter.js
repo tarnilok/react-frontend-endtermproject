74564206adcc2c64264aa746dc5354937c851664
tarnilok <tarnilok@gmail.com>
2021-10-06 13:13:33 +0300

    taking props with destructing method

--- src/router/PrivateRouter.js
+++ src/router/PrivateRouter.js
@@ -1,15 +1,11 @@
-import React, {useContext} from 'react';
-import { AuthContext } from '../context/AuthContext';
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { Redirect, Route } from "react-router-dom";
 
-const PrivateRouter = (props) => {
-    const {currentUser} = useContext(AuthContext)
-    
-    return currentUser ? (
-        <Route path={props.path} component={props.component}/>
-      ) : (
-        <Redirect to="/login" />
-      );
-}
+const PrivateRouter = ({path, component}) => {
+  const { currentUser } = useContext(AuthContext);
 
-export default PrivateRouter
+  return currentUser ? <Route path={path} component={component} /> : <Redirect to="/login" />;
+};
+
+export default PrivateRouter;
